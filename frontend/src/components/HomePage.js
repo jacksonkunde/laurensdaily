2718ed474a324633330785e1cd89d900e7ee499a
Jackson Kunde <newemployee@Ohalos-MacBook-Pro.local>
2024-07-26 16:32:28 -0700

    attempting to fix build issues with package-lock and package

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -3,33 +3,37 @@ import axios from 'axios';
 import './HomePage.css';
 
 const HomePage = () => {
-  const [title, setTitle] = useState('');
+  const [comicData, setComicData] = useState({ title: '', image_url: '' });
+  const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
 
   useEffect(() => {
-    axios.get('http://localhost:5002/comic')
+    axios.get(`${apiBaseUrl}/`)
       .then(response => {
-        setTitle(response.data.title);
+        const { title, image_url } = response.data;
+        setComicData({ title, image_url });
       })
       .catch(error => {
-        console.error('Error fetching the comic title:', error);
+        console.error('Error fetching the comic data:', error);
       });
-  }, []);
+  }, [apiBaseUrl]);
 
   return (
     <div className="container">
       <h1 className="title">Lauren's Daily</h1>
-      {title && <p className="comicTitle">{title}</p>}
+      {comicData.title && <p className="comicTitle">{comicData.title}</p>}
       <div className="comicWrapper">
-        <img
-          src="http://localhost:5002/daily_comic"
-          alt={title}
-          className="comic"
-          onError={(e) => {
-            console.error('Error loading comic image:', e);
-            e.target.style.display = 'none';
-          }}
-          onLoad={() => console.log('Comic image loaded successfully')}
-        />
+        {comicData.image_url && (
+          <img
+            src={`${comicData.image_url}`}
+            alt={comicData.title}
+            className="comic"
+            onError={(e) => {
+              console.error('Error loading comic image:', e);
+              e.target.style.display = 'none';
+            }}
+            onLoad={() => console.log('Comic image loaded successfully')}
+          />
+        )}
       </div>
       <p className="signature">Love, Jackson</p>
     </div>
